b9af3f411518b0c0380ab3cc17375924a0ee5473
João Antonio Soares dos Passos <jdospassos8@gmail.com>
2022-08-28 23:33:54 -0300

    feat: ajuste no http client module

--- users-crud/src/app/core/core.module.ts
+++ users-crud/src/app/core/core.module.ts
@@ -1,17 +1,20 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { CoreRoutingModule } from './core-routing.module';
 import { CoreComponent } from './core.component';
-
+import { TokeInterceptor } from './interceptors/token.interceptor';
 
 @NgModule({
-  declarations: [
-    CoreComponent
+  declarations: [CoreComponent],
+  imports: [HttpClientModule, CommonModule, CoreRoutingModule],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokeInterceptor,
+      multi: true,
+    },
   ],
-  imports: [
-    CommonModule,
-    CoreRoutingModule
-  ]
 })
-export class CoreModule { }
+export class CoreModule {}
